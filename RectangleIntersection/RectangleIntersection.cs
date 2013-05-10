9da79c3f51ba008751a3d3f8b9dd17443403bb80
Lubos Kato <lubos.kato@gmail.com>
2013-05-10 19:00:32 +0200

    New project

---
+++ RectangleIntersection/RectangleIntersection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using RectangleIntersection.Extensions;
+
+[assembly: InternalsVisibleTo("RectangleIntersectionTest")]
+namespace RectangleIntersection
+{
+    public class Vector2
+    {
+        public float X;
+        public float Y;
+        public static Vector2 Zero;
+
+        public Vector2(float unitX, float unitY)
+        {
+            X = unitX;
+            Y = unitY;
+        }
+    }
+
+    internal class RectangleIntersect
+    {
+        public static int Solution(int blx1, int bly1, int trx1, int try1, int blx2, int bly2, int trx2, int try2)
+        {
+            var rectangle1 = new Rectangle(blx1, bly1, trx1 - blx1, try1 - bly1);
+            var rectangle2 = new Rectangle(blx2, bly2, trx2 - blx2, try2 - bly2);
+            //TOIMPLEMENT: add positioning logic for both rectangles !!!
+            Vector2 vector = rectangle1.GetIntersectionDepth(rectangle2);
+            float area = CalculateArea(vector);
+            return (int)area;
+        }
+
+        private static float CalculateArea(Vector2 vector)
+        {
+            if (vector != null)
+            {
+                return vector.X * vector.Y;
+            }
+            return 0;
+        }
+
+        private static void Main()
+        {
+            int area = Solution(0, 2, 5, 10, 3, 1, 20, 15);
+            if (area == 0)
+                Console.WriteLine("There is no intersection");
+            Console.WriteLine("Intersection area is " + area);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RectangleIntersectionTest/RectangleIntersectionTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using RectangleIntersection;
+
+namespace RectangleIntersectionTest
+{
+    [TestFixture]
+    public class RectangleIntersectionTest
+    {
+        [Test]
+        public void TestIntersection()
+        {
+            int area = RectangleIntersect.Solution(0, 2, 5, 10, 3, 1, 20, 15);
+            Assert.AreEqual(area, 16);
+        }
+    }
+}
